005652667ffa4c5af063b3e31625339735eb4af2
adv003 <advay03@gmail.com>
2023-03-06 11:56:36 +0530

    changed home.html to login.html created flask app to render login.html created requirements.txt which contains all the libraries used.

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("login.html")
